45dd2bf4ad0dd0b7320be275e2279e1c7bf18989
sosualfred <sosualfred@gmail.com>
2022-01-16 21:16:04 +0000

    add model title

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length = 10000)
+
+    def __str__(self):
+        return self.title
